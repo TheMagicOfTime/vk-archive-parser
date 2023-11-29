5e64fb385ba2b5ec54f7a899fe0c3de8ae7de099
TheMagicOfTime <andrii_dmitriev@mail.ru>
2023-11-29 22:20:53 +0300

    Реализован функционал по добавлению отметки к сохраненной картинке Тепреь в конца файла будет добавлена приписка, которая сообщяет, является ли хозяин архива автором сообщение в котором была добавлена картинка Сделано для решения проблемы когда, два собеседника могут в одну секунду прислать сообщение с вложением в виде фотографии, из-за чего они будут иметь одинаковое имя и одна картинка будет перезаписана другой. Так де это добавляет дополнительную мета информацию в названии картинки

--- VK_message_parser.py
+++ VK_message_parser.py
@@ -30,6 +30,13 @@ def parse_time_str(time_stamp):
         time_stamp_str_name = time_stamp_str_name + "_" + str(date_numbers[i])
     return time_stamp_str_name
 
+def is_me_author_message(block):
+    try:
+        block.xpath(".//div[@class='message__header']/a")[0].text
+        return False
+    except IndexError:
+        return True
+    
 def download_images(files_in_folder,):
     global g_files_parsed
     for messages in files_in_folder:
@@ -39,6 +46,7 @@ def download_images(files_in_folder,):
         for block in tree.xpath("//div[@class='item']"):
             try:
                 if block.xpath(".//div[@class='attachment__description']")[0].text == "Фотография":
+                    is_me_author = is_me_author_message(block)
                     base_time_stamp_str_name = parse_time_str(block.xpath(".//div[@class='message__header']/text()"))
                     images_block = block.xpath(".//a[@class='attachment__link']")
                     image_number_in_block = 0
@@ -47,7 +55,7 @@ def download_images(files_in_folder,):
                         if len(images_block) > 1:
                             image_number_in_block += 1
                             time_stamp_str_name = time_stamp_str_name + "_" + str(image_number_in_block)
-                        time_stamp_str_name = os.getcwd() + "/{}/".format(sys.argv[2]) + time_stamp_str_name + ".jpg"
+                        time_stamp_str_name = os.getcwd() + "/{}/".format(sys.argv[2]) + time_stamp_str_name + ("_me" if is_me_author else "_you") + ".jpg"
                         r = requests.get(images.text, stream=True)
                         r.raw.decode_content = True
                         with open(time_stamp_str_name,'wb') as img_f:
